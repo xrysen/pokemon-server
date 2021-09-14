ecaaab597cfcdc14ba6ad9014ca5482c933f6d06
Sean Oyler <seanoyler@Seans-MacBook-Pro.local>
2021-09-14 11:23:45 -0700

    Updates cors method

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.use(
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 let success = "";
 let outcome = "";
 let pokemon = {
