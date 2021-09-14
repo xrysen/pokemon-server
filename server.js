3e821fb74580f6e32a89872784f9109ff254680a
Sean Oyler <seanoyler@Seans-MacBook-Pro.local>
2021-09-14 11:39:59 -0700

    Updates cors method

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const cors = require('cors');
 const multer = require("multer");
 const multipart = multer();
 
+app.all('*', function(req, res, next) {
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors());
 
 app.use(
@@ -18,14 +26,6 @@ app.use(
 );
 
 app.use(express.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 let success = "";
 let outcome = "";
